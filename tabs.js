358d43789a3a8c6f21de1307e832d855faf4d67b
farrenal <farrenal@tcd.ie>
2025-11-24 22:10:41 +0000

    Navbar update

--- tabs.js
+++ tabs.js
@@ -1,27 +1,73 @@
-document.getElementById("defaultOpen").click();
-document.getElementById("mobiledefaultOpen").click();
-kdv=document.getElementById("defaultOpen");
-kdv.classname+=" active";
-Mkdv=document.getElementById("mobiledefaultOpen");
-Mkdv.classname+=" active";
+// Fetch all div's which are parts of the navigation table
+const tabs = document.getElementsByClassName("tabContent");
+const tabButtons = document.getElementsByClassName(`tabButton`);
+const toggleButton = document.getElementsByClassName(`toggleButton`);
+const debugText = document.getElementById("debugText");
 
-function openTab(evt, tabName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+// Make a list of the labels for these div's, e.g. Research, Visualisations, ...
+var tabLabels = [];
+for (var i = 0; i < tabs.length; i++){
+  tabLabels.push(tabs[i].className.slice(11));
 }
+
+// Hides all tabs every time the navbar is clicked and every time the page is loaded
+function closeAllTabs(){
+  for (var i = 0; i < tabs.length; i++){
+    tabs[i].style.display = "none";
+  }
+}
+
+function updateButtons(){
+  for (var i = 0; i < tabLabels.length; i++){
+    tabButtons[i].className = `tabButton ${tabLabels[i]}`;
+  }
+}
+
+function tabClicked(tabClass) {
+  if (!openAll){
+    closeAllTabs();
+  };
+  updateButtons();
+  openTab(tabClass);
+  // openedTabClass = tabClass;
+}
+
+function openTab(tabClass) {
+  // Make opened tab visible
+  const openedTab = document.getElementsByClassName(`tabContent ${tabClass}`);
+  openedTab[0].style.display = "block";
+  // $(openedTab[0]).slideDown();
+  
+  // Change style of opened tab button
+  const openedTabButton = document.getElementsByClassName(`tabButton ${tabClass}`);
+  openedTabButton[0].className = `tabButton ${tabClass} active`;
+
+  // Scroll to top of tab content, i.e. headanchor
+  // document.getElementById(`${tabClass}`).scrollIntoView({behavior: 'smooth'});
+  window.location.href = `#${tabClass}`;
+}
+
+function toggleOpenAll(){
+  if (!openAll){
+    openAll = true;
+    for (var i = 0; i < tabs.length; i++){
+        tabs[i].style.display = "block";
+    };
+    toggleButton[0].className = `toggleButton active`;
+  }
+  else{
+    openAll = false;
+    for (var i = 0; i < tabs.length; i++){
+        tabs[i].style.display = "none";
+    };
+    toggleButton[0].className = `toggleButton`;
+    tabClicked(openedTabClass);
+  }
+}
+
+// Initialise open tab with Research
+var openedTabClass = "Research";
+tabClicked(openedTabClass);
+
+// Start with single tab view
+var openAll = false;
